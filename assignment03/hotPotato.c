5d795b9529ff14414edfaae78c906470c06d7536
RocketSumit <sumitpatidar96@gmail.com>
2016-08-27 12:14:16 +0530

    Queue declared and user input taken

--- assignment03/hotPotato.c
+++ assignment03/hotPotato.c
@@ -4,3 +4,22 @@ Roll no: B15237
 Purpose: IC250 assignment, question 01
 Date: 27/08/2016
 ---------------------------------------------------------------------------------*/
+#include <stdio.h>
+
+typedef struct queue{
+    int front , rear;
+    int *array;
+}children;
+/* Front points to first element in queue,
+    Rear points to last element in queue */
+    
+int Qinsert(children* ptr, int front, int rear, int data);
+int Qdelete(children* ptr, int front, int rear);
+
+int main(){
+    int players;
+    printf("Enter the number of children: ");
+    scanf("%d", &players);
+
+    children *group1; 
+}
