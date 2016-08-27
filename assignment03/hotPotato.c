dcacc8d83250bad04b6a83249106b43cf32d3a24
RocketSumit <sumitpatidar96@gmail.com>
2016-08-27 13:03:08 +0530

    Insertion function added

--- assignment03/hotPotato.c
+++ assignment03/hotPotato.c
@@ -7,13 +7,13 @@ Date: 27/08/2016
 #include <stdio.h>
 
 typedef struct queue{
-    int front , rear;
+    int front , rear, capacity;
     int *array;
 }children;
 /* Front points to first element in queue,
     Rear points to last element in queue */
-    
-int Qinsert(children* ptr, int front, int rear, int data);
+
+int Qinsert(children* ptr, int data);
 int Qdelete(children* ptr, int front, int rear);
 
 int main(){
@@ -21,5 +21,30 @@ int main(){
     printf("Enter the number of children: ");
     scanf("%d", &players);
 
-    children *group1; 
+    children *group1;
+    return 0; 
+}
+
+int Qinsert(children* ptr, int data){
+    // check wheather queue is full
+    if((ptr->front == 0 && (ptr->rear) == (ptr->capacity) - 1) || (ptr->front) == (ptr->rear) + 1)  
+        return -1;
+    
+    // for a non empty queue set rear and front
+    else(){
+        if(ptr->front = -1){
+             ptr->front = 0;
+             ptr->rear = 0;
+        }
+        else if((ptr->rear) = (ptr->capacity - 1)){
+            ptr->rear = 0;
+        }
+        else{
+            (ptr->rear)++;
+        }
+    }
+
+    //insert data to array
+    ptr->array[ptr->rear] = data;
+    return 0;
 }
