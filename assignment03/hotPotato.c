17acda91fefe7066b9a70b2d19b931b7c16b0a7c
RocketSumit <sumitpatidar96@gmail.com>
2016-08-27 13:10:03 +0530

    Create queue is added to hot potato

--- assignment03/hotPotato.c
+++ assignment03/hotPotato.c
@@ -5,6 +5,7 @@ Purpose: IC250 assignment, question 01
 Date: 27/08/2016
 ---------------------------------------------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct queue{
     int front , rear, capacity;
@@ -12,7 +13,7 @@ typedef struct queue{
 }children;
 /* Front points to first element in queue,
     Rear points to last element in queue */
-
+children* createQueue(int capacity);
 int Qinsert(children* ptr, int data);
 int Qdelete(children* ptr, int front, int rear);
 
@@ -21,8 +22,18 @@ int main(){
     printf("Enter the number of children: ");
     scanf("%d", &players);
 
-    children *group1;
+    children *group1 = createQueue(players);
+    
     return 0; 
+}
+
+children* createQueue(int capacity){
+    children *ptr = (children*)malloc(sizeof(children));
+    //initialize queue
+    ptr->capacity = capacity;
+    ptr->rear = ptr->front = -1;
+    /* -1 indicates queue is empty */
+    ptr->array = (int *)malloc(sizeof(int));
 }
 
 int Qinsert(children* ptr, int data){
@@ -31,12 +42,12 @@ int Qinsert(children* ptr, int data){
         return -1;
     
     // for a non empty queue set rear and front
-    else(){
-        if(ptr->front = -1){
+    else{
+        if(ptr->front == -1){
              ptr->front = 0;
              ptr->rear = 0;
         }
-        else if((ptr->rear) = (ptr->capacity - 1)){
+        else if((ptr->rear) == (ptr->capacity - 1)){
             ptr->rear = 0;
         }
         else{
