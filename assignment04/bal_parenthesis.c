407bc5b39123965b3a0d26739d6301511b75fcb2
RocketSumit <sumitpatidar96@gmail.com>
2016-09-03 22:41:27 +0530

    assignment04 directory created

---
+++ assignment04/bal_parenthesis.c
@@ -0,0 +1,17 @@
+/*.........................................................
+   Name: Sumit Patidar
+   Roll no: B15237
+   Purpose: Assignment 04, Q1
+   Date: 03/09/2016
+   .........................................................*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+        char ch;
+
+        while (ch!= EOF || ch!= '\n') {
+                ch = getc();
+        }
+        return 0;
+}
