3f2e9806c6592f80d4038f7951bf1f087788facf
RocketSumit <sumitpatidar96@gmail.com>
2016-09-03 23:06:20 +0530

    simple check for () parenthesis implemented

--- assignment04/bal_parenthesis.c
+++ assignment04/bal_parenthesis.c
@@ -5,14 +5,24 @@
    Date: 03/09/2016
    .........................................................*/
 
-#include <stdio.h>
+#include "my_library.h"
 
 int main(int argc, char const *argv[]) {
         char ch;
+        GSList **stack = NULL;
 
         while (ch!= EOF || ch!= '\n') {
                 ch = getc(stdin);
+                if(ch == '(') {
+                        stackInsert(stack, ch);
+                }
 
+                else if (ch == ')') {
+                        if (stackRemove(stack) == NULL) {
+                                printf("Unbalaced\n");
+                                return 0;
+                        }
+                }
         }
         return 0;
 }
